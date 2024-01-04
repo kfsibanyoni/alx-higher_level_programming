2dc4b6e914ca03c909f3a0eb30cf53869190b6b2
kfsibanyoni <kfsibanyoni@gmail.com>
2024-01-04 20:46:13 +0200

    module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,8 +2,8 @@
 
 if __name__ == "__main__":
 
-   import add_0
+   from add_0 import add
 
    a = 1
    b = 2
-   print(f'{a} + {b} = {a+b}')
+   print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
