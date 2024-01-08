a85b07c0f07980294e43278b35549dd7de4c8623
kfsibanyoni <kfsibanyoni@gmail.com>
2024-01-08 19:19:14 +0200

    use return

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def element_at(my_list, idx):
-    if idx < 0 or idx > len:
+    if idx < 0 or idx > len(my_list):
         print("None")
     else:
-        print('{:d}'.format(idx))
+        return my_list[idx]
