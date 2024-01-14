d7e88a19048cf708f8864450bef1c21492a5fa93
kfsibanyoni <kfsibanyoni@gmail.com>
2024-01-14 18:21:23 +0200

    more ds

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def uniq_add(my_list=[]):
+    result = 0
+    uniq = []
+    for i in my_list:
+        if i in uniq:
+            continue
+        else:
+            uniq.append(i)
+    
+    for element in uniq:
+            result += element
+    return (result)
