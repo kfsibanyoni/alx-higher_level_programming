60a1043f6e4fbc0e5b87110b803d3f68502c6f58
kfsibanyoni <kfsibanyoni@gmail.com>
2024-01-14 18:00:53 +0200

    search through range

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def search_replace(my_list, search, replace):
-    for x in my_list:
-         new_list = my_list.pop[search].append[replace]
+    new_list = my_list.copy()
+    for i in range(len(my_list)):
+        if my_list[i] == search:
+            new_list[i] = replace
     return (new_list)
